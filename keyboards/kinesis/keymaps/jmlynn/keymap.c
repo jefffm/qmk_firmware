b891a3aa4e9c21227a3ebc9ac5e3cbf783f6e5fb
Jeff Lynn <4128043+jefffm@users.noreply.github.com>
2023-07-13 09:25:09 -0400

    adjust kinesis keymap

--- keyboards/kinesis/keymaps/jmlynn/keymap.c
+++ keyboards/kinesis/keymaps/jmlynn/keymap.c
@@ -3,13 +3,14 @@
 #define _DVORAK 0 // Base Dvorak layer
 #define _MEDIA 1  // Media layer
 
+// clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_DVORAK] = LAYOUT(
         // left hand
-        KC_ESC,   KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_F5, KC_F6, KC_F7, KC_F8,
+        KC_CAPS,   KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_F5, KC_F6, KC_F7, KC_F8,
         KC_EQL,   KC_1,    KC_2,    KC_3,    KC_4,   KC_5,
         KC_TAB,   KC_QUOT, KC_COMM, KC_DOT,  KC_P,   KC_Y,
-        KC_CAPS,  KC_A,    KC_O,    KC_E,    KC_U,   KC_I,
+        KC_ESC,  KC_A,    KC_O,    KC_E,    KC_U,   KC_I,
         KC_LSFT,  KC_SCLN, KC_Q,    KC_J,    KC_K,   KC_X,
                   KC_GRV,  KC_INS, KC_LEFT, KC_RIGHT,
         // left thumb
@@ -54,3 +55,4 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_TRNS, KC_ENT, KC_NO
 )
 };
+// clang-format on
