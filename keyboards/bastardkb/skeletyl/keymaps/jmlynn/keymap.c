0474a37801fb02b68186864fffea05586fb9d653
Jeff Lynn <4128043+jefffm@users.noreply.github.com>
2023-07-17 19:42:32 -0400

    [skeletyl] bring layout up to parity

--- keyboards/bastardkb/skeletyl/keymaps/jmlynn/keymap.c
+++ keyboards/bastardkb/skeletyl/keymaps/jmlynn/keymap.c
@@ -1,15 +1,11 @@
 #include QMK_KEYBOARD_H
 
-#include "swapper.h"
-
-#define HOME G(KC_LEFT)
-#define END G(KC_RGHT)
-#define FWD G(KC_RBRC)
-#define BACK G(KC_LBRC)
-#define TABL G(S(KC_LBRC))
-#define TABR G(S(KC_RBRC))
-#define SPCL A(G(KC_LEFT))
-#define SPC_R A(G(KC_RGHT))
+enum layers {
+    DEF,
+    SYM,
+    NAV,
+    NUM,
+};
 
 // layers
 #define LA_SYM MO(SYM)
@@ -25,18 +21,19 @@
 #define OS_RCTL OSM(MOD_RCTL)
 #define OS_RGUI OSM(MOD_RGUI)
 
-enum layers {
-    DEF,
-    SYM,
-    NAV,
-    NUM,
-};
-
-enum keycodes {
-    // Custom oneshot mod implementation with no timers.
-    SW_WIN = SAFE_RANGE,
-    SW_LANG, // Switch to next input language (gui-spc)
-};
+// misc
+#define SSHOT SGUI(KC_S)
+#define UNDO C(KC_Z)
+#define REDO C(S(KC_Z))
+#define CUT C(KC_X)
+#define COPY C(KC_C)
+#define PASTE C(KC_V)
+#define TAB_CLS C(KC_W)
+#define TAB_NEW C(KC_T)
+#define TAB_PREV C(S(KC_TAB))
+#define TAB_NEXT C(KC_TAB)
+#define BACK KC_WBAK
+#define FWD KC_WFWD
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -66,11 +63,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [NAV] = LAYOUT_split_3x5_3(
   //┌────────┬────────┬────────┬────────┬────────┐  ┌────────┬────────┬────────┬────────┬────────┐
-     KC_TAB  ,SW_WIN  ,TABL    ,TABR    ,KC_VOLU ,   QK_BOOT, HOME    ,KC_UP   ,END     ,KC_DEL  ,
+     REDO    , SSHOT  , COPY   , PASTE  , CUT    ,   KC_TAB  ,KC_HOME , KC_UP  , KC_END , KC_DEL ,
   //├────────┼────────┼────────┼────────┼────────┤  ├────────┼────────┼────────┼────────┼────────┤
-     OS_LGUI ,OS_LALT ,OS_LCTL ,OS_LSFT ,KC_VOLD ,   CW_TOGG ,KC_LEFT ,KC_DOWN ,KC_RGHT ,KC_BSPC ,
+     OS_LGUI ,OS_LALT ,OS_LCTL ,OS_LSFT , XXXXXXX,   CW_TOGG ,KC_LEFT ,KC_DOWN ,KC_RGHT ,KC_BSPC ,
   //├────────┼────────┼────────┼────────┼────────┤  ├────────┼────────┼────────┼────────┼────────┤
-     SPCL    , SPC_R  , BACK   , FWD    ,KC_MPLY ,   XXXXXXX ,KC_PGDN ,KC_PGUP ,SW_LANG ,KC_ENT  ,
+     UNDO    , TAB_NEW,TAB_PREV,TAB_NEXT,TAB_CLS ,   XXXXXXX ,KC_PGDN ,KC_PGUP , BACK   , FWD    ,
   //└────────┴────────┼────────┼────────┼────────┤  ├────────┼────────┼────────┼────────┴────────┘
                         _______, _______, _______,    _______ ,_______,KC_ENT
   //                  └────────┴────────┴────────┘  └────────┴────────┴────────┘
@@ -80,7 +77,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //┌────────┬────────┬────────┬────────┬────────┐  ┌────────┬────────┬────────┬────────┬────────┐
      KC_1    ,KC_2    ,KC_3    ,KC_4    ,KC_5    ,   KC_6    ,KC_7    ,KC_8    ,KC_9    ,KC_0    ,
   //├────────┼────────┼────────┼────────┼────────┤  ├────────┼────────┼────────┼────────┼────────┤
-     KC_LGUI ,KC_LALT ,KC_LCTL ,KC_LSFT ,XXXXXXX ,   XXXXXXX ,OS_RSFT ,OS_RCTL ,OS_RALT ,OS_RGUI,
+     KC_LGUI ,KC_LALT ,KC_LCTL ,KC_LSFT ,KC_F11  ,   KC_F12  ,OS_RSFT ,OS_RCTL ,OS_RALT ,OS_RGUI,
   //├────────┼────────┼────────┼────────┼────────┤  ├────────┼────────┼────────┼────────┼────────┤
      KC_F1   ,KC_F2   ,KC_F3   ,KC_F4   ,KC_F5   ,   KC_F6   ,KC_F7   ,KC_F8   ,KC_F9   ,KC_F10,
   //└────────┴────────┼────────┼────────┼────────┤  ├────────┼────────┼────────┼────────┴────────┘
